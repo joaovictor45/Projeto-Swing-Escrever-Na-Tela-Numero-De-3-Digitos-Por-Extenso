4c9e82fef6cba5a403d762c9c3bd51f49b8ac3d1
João Victor <Joo Victor@DESKTOP-CIQV661>
2022-12-03 21:35:11 -0300

    VersãoFinal

--- EscreverNaTelaNumeroDe3DigitosPorExtenso/src/model/Testes.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Testes {
-
-    public static void main(String[] args) {
-        EscreverPorExtenso3Digito escrever = new EscreverPorExtenso3Digito();
-        System.out.println(escrever.escrever(221));
-    }
-}
